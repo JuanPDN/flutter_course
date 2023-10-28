aa062a3d32fed977fbca344b5ef82da30bccd506
Juan Pablo <juanpablo@MacBook-Air-de-Juan.local>
2023-10-28 17:32:33 -0500

    first commit curse flutter

--- 01-Dart-intro/02_Variables.dart
+++ 01-Dart-intro/02_Variables.dart
@@ -5,12 +5,23 @@ void maim(){
     final bool isAlive = true
     final List<String> abilities = ['impostor']
     final sprites = <String>['ditto/front.pn']
+
+    //  dynamic == null
+
+    dynamic erroMessage = 'Hola'
+    erroMessage = true
+    erroMessage = [1,2,3,4,5,6]
+    erroMessage = { 1,2,3,4,5,6}
+    erroMessage = () => true
+    erroMessage = null
+
   print("""
   $pokemon
   $hp
   $isAlive
   $habilities
   $sprites
+  $erroMessage
   """)
 
 }
