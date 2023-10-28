f82f550f6d5698402abd819cb1a2b4bf63ea43a1
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2023-10-28 18:05:05 -0500

    first commit curse flutter

--- 01-Dart-intro/02_Variables.dart
+++ 01-Dart-intro/02_Variables.dart
@@ -1,6 +1,6 @@
 void main(){
 
-    final String pokemon = 'Ditto';
+    final String pokemon = 'ditto';
     final int hp = 100;
     final bool isAlive = true;
     final List<String> abilities = ['impostor'];
