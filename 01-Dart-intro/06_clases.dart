1e06df9ae981740088714fc6bf52432bd539435b
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2023-10-31 16:25:09 -0500

    Class dart

--- 01-Dart-intro/06_clases.dart
+++ 01-Dart-intro/06_clases.dart
@@ -10,8 +10,8 @@ class Hero {
   String name;
   String power;
 
-  //Hero(this.name, this.power);
-  Hero (String pName, String pPower)
-    : name = pName,
-      power = pPower;
+  Hero(this.name, this.power);
+  // Hero (String pName, String pPower)
+  //   : name = pName,
+  //     power = pPower;
 }
