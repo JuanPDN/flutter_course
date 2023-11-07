7790db50d93ca5f1bae9e855a1fe59b2bd77da70
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2023-11-07 17:42:53 -0500

    First app flutter

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -10,9 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CouterScreen()
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue
+      ),
+      home: const CouterScreen()
     );
   }
   
