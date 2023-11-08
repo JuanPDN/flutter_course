34a7677e74aab715283c0c97a7c1a33d81dbc905
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2023-11-08 14:43:05 -0500

    First app flutter

--- hello_world_app/lib/presentation/screens/Counter/counter_screen.dart
+++ hello_world_app/lib/presentation/screens/Counter/counter_screen.dart
@@ -1,28 +1,39 @@
 import 'package:flutter/material.dart';
 
-class CouterScreen extends StatelessWidget {
+class CouterScreen extends StatefulWidget {
   const CouterScreen({super.key});
 
+  @override
+  State<CouterScreen> createState() => _CouterScreenState();
+}
+
+class _CouterScreenState extends State<CouterScreen> {
+  int clickCounter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
       ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('10', style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
-              Text('Clicks', style: TextStyle(fontSize: 25))
-            ],
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$clickCounter',
+                style: const TextStyle(
+                    fontSize: 160, fontWeight: FontWeight.w100)),
+            const Text('Clicks', style: TextStyle(fontSize: 25))
+          ],
         ),
-        floatingActionButton: FloatingActionButton(onPressed: (){
-
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            clickCounter += 1;
+          });
         },
-        child: const Icon( Icons.plus_one),
-        ) ,
-      );
+        child: const Icon(Icons.plus_one),
+      ),
+    );
   }
 }
