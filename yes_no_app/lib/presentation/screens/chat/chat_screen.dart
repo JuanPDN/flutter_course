a9b5c97d648aa9fc870e8099ddf368bafdc2e522
Juan Delgado Nivia <juandelgadonivia@MacBook-Air-de-Juan.local>
2023-12-28 15:41:55 -0500

    message field box

--- yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/Shared/message_field_box.dart';
 import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 
@@ -36,9 +37,12 @@ class _ChatView extends StatelessWidget {
                 child: ListView.builder(
               itemCount: 100,
               itemBuilder: (context, index) {
-                return (index % 2 == 0 ? const HerMessageBubble(): const MyMessageBubble());
+                return (index % 2 == 0
+                    ? const HerMessageBubble()
+                    : const MyMessageBubble());
               },
-            ))
+            )),
+            const MessageFieldBox(),
           ],
         ),
       ),
