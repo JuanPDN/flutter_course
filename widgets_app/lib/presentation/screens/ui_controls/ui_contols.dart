ecce32932d6110c1fba8d9163abb212584b60fa1
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-01-10 17:52:55 -0500

    screens creadas y barril actualizado

---
+++ widgets_app/lib/presentation/screens/ui_controls/ui_contols.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UiContolsScreen extends StatelessWidget {
+
+  static const String name = 'ui_controls_screen';
+  
+  const UiContolsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
