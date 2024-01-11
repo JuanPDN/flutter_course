bd22f18e21729d3530e420d8d918aebf7862ca7f
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-01-11 17:38:50 -0500

    snackbars finished

--- widgets_app/lib/config/menu/menu_items.dart
+++ widgets_app/lib/config/menu/menu_items.dart
@@ -25,8 +25,14 @@ const List<MenuItem> appMenuItem = [
       link: '/cards',
       icon: Icons.credit_card),
   MenuItem(
-      title: 'ProgressIndicators',
+      title: 'Progress Indicators',
       subTitle: 'Generales y controlados',
       link: '/progress',
-      icon: Icons.refresh_rounded)
+      icon: Icons.refresh_rounded),
+  MenuItem(
+      title: 'Snackbar y dialogos',
+      subTitle: 'indicadores en pantalla',
+      link: '/snackbars',
+      icon: Icons.info_outline),
+
 ];
---
+++ widgets_app/lib/presentation/screens/snackbars/snackbar_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SnackbarScreen extends StatelessWidget {
+  static const String name = 'snackbar_screen';
+
+  const SnackbarScreen({super.key});
+
+  void showCustomSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackBar = (SnackBar(
+      content: const Text('Hola mundo'),
+      action: SnackBarAction(label: 'Ok!', onPressed: () {}),
+      duration: const Duration(seconds: 2),
+    ));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbars y dialogos'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Mostrar Snackbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () => showCustomSnackbar(context),
+      ),
+    );
+  }
+}
