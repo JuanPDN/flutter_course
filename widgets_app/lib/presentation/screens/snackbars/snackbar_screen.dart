7e592a973af3013e411fc56e71545be42438c0a0
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-01-12 15:48:12 -0500

    snacbars dialogs finshed

--- widgets_app/lib/presentation/screens/snackbars/snackbar_screen.dart
+++ widgets_app/lib/presentation/screens/snackbars/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const String name = 'snackbar_screen';
@@ -16,11 +17,50 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('¿Estas seguro?'),
+        content: const Text(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam finibus aliquet ligula dapibus suscipit. Proin rutrum ante eu pharetra laoreet. Ut non nisl id erat commodo pretium non vel nibh.'),
+        actions: [
+          TextButton(
+              onPressed: () {
+                context.pop();
+              },
+              child: const Text('Cancelar')),
+          FilledButton(
+              onPressed: () => context.pop(), child: const Text('Aceptar'))
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbars y dialogos'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+                onPressed: () {
+                  showAboutDialog(context: context, children: [
+                    const Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam finibus aliquet ligula dapibus suscipit. Proin rutrum ante eu pharetra laoreet. Ut non nisl id erat commodo pretium non vel nibh. Fusce gravida blandit facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget sapien vitae metus dignissim tincidunt. Nunc consectetur hendrerit risus, faucibus bibendum enim vehicula eu. Sed vehicula fermentum ultricies. Sed venenatis purus sed purus dignissim, ut molestie diam sollicitudin. Pellentesque gravida sem sit amet sem accumsan aliquam. Nulla malesuada scelerisque rutrum. Donec non leo at purus venenatis blandit in eget libero.')
+                  ]);
+                },
+                child: const Text('Licencias usadas')),
+            FilledButton.tonal(
+                onPressed: () => openDialog(context),
+                child: const Text('Mostrar Dialogo'))
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Mostrar Snackbar'),
