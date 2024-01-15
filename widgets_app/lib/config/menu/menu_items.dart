6f74ec688c192e066a85b0da94b6c276044cc171
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-01-15 17:36:25 -0500

    counter riverpod

--- widgets_app/lib/config/menu/menu_items.dart
+++ widgets_app/lib/config/menu/menu_items.dart
@@ -14,6 +14,11 @@ class MenuItem {
 }
 
 const List<MenuItem> appMenuItem = [
+    MenuItem(
+      title: 'Riverpod Counter',
+      subTitle: 'introduccions a riverpod',
+      link: '/counter-screen',
+      icon: Icons.add),
   MenuItem(
       title: 'Botones',
       subTitle: 'Varios botones en flutter',
@@ -54,5 +59,4 @@ const List<MenuItem> appMenuItem = [
       subTitle: 'Listas infinitas y pull to refresh',
       link: '/infinite_scroll',
       icon: Icons.list_alt_rounded),
-
 ];
--- widgets_app/lib/config/router/app_router.dart
+++ widgets_app/lib/config/router/app_router.dart
@@ -47,4 +47,9 @@ final appRouter = GoRouter(routes: [
     name: InfiniteScrollScreen.name,
     builder: (context, state) => const InfiniteScrollScreen(),
   ),
+  GoRoute(
+    path: '/counter-screen',
+    name: CounterScreen.name,
+    builder: (context, state) => const CounterScreen(),
+  ),
 ]);
