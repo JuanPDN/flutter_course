bcd94515b3aa6dea48de47f976c14c546f2a8527
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-01-20 17:24:20 -0500

    providers TMDB

--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart'
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
 
@@ -25,17 +24,14 @@ class _HomeView extends ConsumerStatefulWidget {
 }
 
 class _HomeViewState extends ConsumerState<_HomeView> {
-
   @override
   void initState() {
     super.initState();
-    
     ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
   }
 
   @override
   Widget build(BuildContext context) {
-
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
     return ListView.builder(
