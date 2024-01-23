db498dc983c619f29f1f2f7066331a6fdd37d31a
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-01-23 18:25:47 -0500

    movies horizontal

--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -5,8 +5,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 
-
-
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
 
@@ -38,16 +36,18 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-
-    // final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final slideShowMovies = ref.watch(moviesSlideshowProvider);
 
     return Column(
       children: [
         const CustomAppbar(),
-
-        MoviesSlideShow(movies: slideShowMovies)
-        
+        MoviesSlideShow(movies: slideShowMovies),
+        MovieHorizontalListView(
+          movies: nowPlayingMovies,
+          title: 'En cines',
+          subTitle: 'Lunes 22',
+        ),
       ],
     );
   }
--- cinemapedia/lib/presentation/widgets/widgets.dart
+++ cinemapedia/lib/presentation/widgets/widgets.dart
@@ -1,6 +1,8 @@
-export 'package:cinemapedia/presentation/widgets/shared/custom_navigationbar.dart';
+export 'package:cinemapedia/presentation/widgets/movies/movie_horizontal_listview.dart';
 export 'package:cinemapedia/presentation/widgets/movies/movies_slideshow.dart';
 export 'package:cinemapedia/presentation/widgets/shared/custom_appbar.dart';
+export 'package:cinemapedia/presentation/widgets/shared/custom_navigationbar.dart';
+
 
 
 
