36557b22f18a3f2582541b1b6c6bac76d20dd819
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-01-25 17:52:09 -0500

    actualizacion dependencias

---
+++ cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MovieScreen extends StatelessWidget {
+  const MovieScreen({super.key, required this.movieId});
+
+  static const name = 'movie-screen';
+
+  final String movieId;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
