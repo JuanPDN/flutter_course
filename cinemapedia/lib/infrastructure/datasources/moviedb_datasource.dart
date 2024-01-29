72be7c8443e49ec3c26c07436e969aaad3256223
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-01-28 20:00:22 -0500

    Screen movie

--- cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
+++ cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:dio/dio.dart';
 
 import 'package:cinemapedia/config/constants/environment.dart';
@@ -37,18 +38,30 @@ class MoviedbDatasource extends MoviesDatasource {
         await dio.get('/movie/popular', queryParameters: {'page': page});
     return _jsonToMovies(response.data);
   }
-  
+
   @override
-  Future<List<Movie>> getTopRating({int page = 1}) async{
-   final response =
+  Future<List<Movie>> getTopRating({int page = 1}) async {
+    final response =
         await dio.get('/movie/top_rated', queryParameters: {'page': page});
     return _jsonToMovies(response.data);
   }
-  
+
   @override
-  Future<List<Movie>> getUpComing({int page = 1}) async{
-   final response =
+  Future<List<Movie>> getUpComing({int page = 1}) async {
+    final response =
         await dio.get('/movie/upcoming', queryParameters: {'page': page});
     return _jsonToMovies(response.data);
+  }
+
+  @override
+  Future<Movie> getMovieById(String id) async {
+    final response = await dio.get('/movie/$id');
+    if (response.statusCode != 200) throw Exception('Movie Not Found');
+    
+    final movieDetails = MovieDetails.fromJson(response.data);
+
+    final Movie movie = MovieMapper.movieDetailsToEntity(movieDetails);
+
+    return movie;
   }
 }
