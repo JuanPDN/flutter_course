be0a33dbab0e3f1cd8e01482d9b875eca3180661
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-02-03 16:15:41 -0500

    view de favorites

--- cinemapedia/lib/presentation/screens/movies/movie_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -204,8 +204,9 @@ class _CustomSliverAppBar extends ConsumerWidget {
       foregroundColor: Colors.white,
       actions: [
         IconButton(
-            onPressed: () {
-              ref.watch(localStorageRepositoryProvider).toggleFavorite(movie);
+            onPressed: () async{
+              await ref.read(favoriteMoviesProvider.notifier).toggleFavorite(movie);
+              // ref.read(localStorageRepositoryProvider).toggleFavorite(movie);
               ref.invalidate(isFavoriteProvider(movie.id));
             },
             icon: isFavoriteFuture.when(
